5cc747e72b3a15b3f355fdeee70a726728ce2665
Ahmad Bondan Gunawan <bondangunawan77@gmail.com>
2024-06-21 23:45:30 +0700

    modified

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,11 +1,39 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const MovieDetail = ({ navigation }: any): JSX.Element => {
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    const ACCESS_TOKEN = "YOUR_API_TOKEN";
+
+    const url =
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(url, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.marginBottom20}>Movie Detail Page</Text>
-      <Button title="Kembali" onPress={() => navigation.goBack()} />
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData();
+        }}
+      />
     </View>
   );
 };
