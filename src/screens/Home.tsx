34fc10e0551b8adbd4e7e2cf05c7dd2953fa81b2
Ahmad Bondan Gunawan <bondangunawan77@gmail.com>
2024-06-24 20:09:08 +0700

    develop_home_screen_layout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,46 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, ScrollView, View, StatusBar } from "react-native";
 import React from "react";
+import type { MovieListProps } from "../types/app";
+import MovieList from "../components/movies/MovieList";
 
-const Home = ({ navigation }: any): JSX.Element => {
+const movieLists: MovieListProps[] = [
+  {
+    title: "Now Playing in Theater",
+    path: "movie/now_playing?language=en-US&page=1",
+    coverType: "backdrop",
+  },
+  {
+    title: "Upcoming Movies",
+    path: "movie/upcoming?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Top Rated Movies",
+    path: "movie/top_rated?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Popular Movies",
+    path: "movie/popular?language=en-US&page=1",
+    coverType: "poster",
+  },
+];
+
+const Home = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.marginBottom20}>Movie Page</Text>
-      <Button
-        title="Pergi Ke MovieDetail"
-        onPress={() => navigation.navigate("MovieDetail")}
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -17,11 +48,8 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  marginBottom20: {
-    marginBottom: 20,
+    rowGap: 16,
   },
 });
